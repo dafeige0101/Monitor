882f10e4621acba47f3c56b39531351cc2af934b
luomu <65743350+baquanluomu@users.noreply.github.com>
2022-04-16 20:45:05 +0800

    Update and rename jd_team60.js to jd_wdz.js

--- jd_wdz.js
+++ jd_wdz.js
@@ -13,7 +13,7 @@
 function openCardActivity(activityId, activityUrl, pin, num, againUserIndex) {
   return new Promise((resolve) => {
     const prefix = activityUrl.includes("cjhydz") ? "cjhydz" : "lzkjdz";
-    const $ = new Env("微定制瓜分京豆");
+    const $ = new Env("微定制瓜分京豆-落幕");
     const notify = $.isNode() ? require("./sendNotify") : "";
     const jdCookieNode = $.isNode() ? require("./jdCookie.js") : "";
     let cookiesArr = [],
